23e8d3c489113c6e6f0ba5d6fe5426a2b00b63ad
Andrew I. Schein <setjmp@google.com>
2015-04-30 07:57:57 -0700

    back up current version of jmp.sh in preparation to replace xargs with parrellel.

--- jmp.sh
+++ jmp.sh
@@ -43,10 +43,29 @@ setjmp () {
   JMP_BOOKMARKS[$1]="$2"
 }
 
+_setjmp_getroot() {
+  if [ ! $JMP_ROOT_HINT ] ; then
+    echo 1>&2 "\$JMP_ROOT_HINT must be defined!"
+    return 1
+  fi
+  if ! (echo `pwd` | grep "${JMP_ROOT_HINT}")  > /dev/null ; then
+    #TODO: this condition can disapear by using getroot()
+    if [ -e "./${JMP_ROOT_HINT}" ] ; then
+      #we are one level higher than the root hint 
+      echo `pwd`"/${JMP_ROOT_HINT}"
+      return 0
+    fi
+    echo 1>&2 "must be within $JMP_ROOT_HINT directory or it must exists in the current directory."
+    return 1
+  fi
+  result=`pwd`
+  echo ${result%%${JMP_ROOT_HINT}*}${JMP_ROOT_HINT}
+}
+
 jmp () {
   #jmp bookmark_label
   #cd to path under current $jmp_root directory indicated by label
-  if [ ! $JMP_ROOT_HINT ] ; then
+ if [ ! $JMP_ROOT_HINT ] ; then
     echo 1>&2 "\$JMP_ROOT_HINT must be defined!"
     return 1
   fi
@@ -56,6 +75,7 @@ jmp () {
   fi
   local dir=`pwd`
   if ! (echo `pwd` | grep "${JMP_ROOT_HINT}")  > /dev/null ; then
+    #TODO: this condition can disapear by using getroot() 
     if [ -e "./${JMP_ROOT_HINT}" ] ; then
       #we are one level higher than the root hint
       cd "./${JMP_ROOT_HINT}/${JMP_BOOKMARKS[$1]}"
@@ -76,3 +96,22 @@ jmp () {
 
 # in case you are into the whole brevity thing.
 alias j='jmp'
+
+# tool to enumerate files in the directories of JMP_BOOKMARKS
+# at first, let's assume we are at root
+jfind () {
+  root=`_setjmp_getroot`
+  echo "root is: $root"
+  (for dir in "${JMP_BOOKMARKS[@]}" ; do echo $dir; done;) | \
+    xargs -I {} find {} -maxdepth 1 -type f ;
+}
+
+# tool to enumerate files in the blaze-bin output directories
+# corresponding to JMP_BOOKMARKS.  We asume PWD is root
+bfind () {
+  root=`_setjmp_getroot`
+  echo "root is: $root"
+  (for dir in "${JMP_BOOKMARKS[@]}" ; do echo $dir; done;) | \
+    xargs -P 16 -I {} find blaze-bin/{} -maxdepth 1 -type f ;
+}
+
